cb2e978c0476e632b36119de2d7d980192cfb1e9
mkaf3 <95512118+mkaf3@users.noreply.github.com>
2022-03-22 19:51:21 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+    var canvas = new fabric.Canvas('myCanvas');
+
+
+    block_image_width = 30;
+    block_image_height = 30;
+
+
+    player_x = 10;
+    player_y = 10;
+
+
+    var player_object= "";
+
+
+
+    function player_update()
+    {
+        fabric.Image.fromURL("player.png",  function(Img) {
+        player_object = Img;
+
+
+        player_object.scaleToWidth(150);
+        player_object.scaleToWidth(140);
+        player_object.set({
+            top:player_y,
+            left:player_x
+        });
+        canvas.add(player)
+
+
+
+        }
+    }
